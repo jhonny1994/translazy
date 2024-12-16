54e63c6f854fabc0fa1a2cee1936cf85e6638515
mohamed abderraouf douadi <me.is.intik@gmail.com>
2024-12-16 08:49:02 +0100

    added device preview screenshot

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,10 @@
 import 'dart:io';
 
-import 'package:device_preview/device_preview.dart';
+import 'package:device_preview_screenshot/device_preview_screenshot.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:path_provider/path_provider.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 import 'package:translazy/core/localization/generated/l10n.dart';
 import 'package:translazy/core/theme.dart';
@@ -17,13 +18,19 @@ import 'package:translazy/providers/theme_notifier_provider.dart';
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   final sharedPreferences = await SharedPreferences.getInstance();
-
+  final appDir = await getApplicationDocumentsDirectory();
   runApp(
     ProviderScope(
       overrides: [
         sharedPreferencesProvider.overrideWithValue(sharedPreferences),
       ],
       child: DevicePreview(
+        tools: [
+          ...DevicePreview.defaultTools,
+          DevicePreviewScreenshot(
+            onScreenshot: screenshotAsFiles(appDir),
+          ),
+        ],
         enabled: Platform.isWindows,
         builder: (context) => const MainApp(),
       ),
